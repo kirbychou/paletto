35b992dd296ca660b3172e4418b325973bc4de7a
carpentier lucie <lcarpent@prune>
2014-10-23 15:07:32 +0200

    first commit

--- test/test.js
+++ test/test.js
@@ -3,4 +3,8 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var Engine = new Engine();
+    assertEquals(Engine.check_length_board(),36);
+    assertNotEqual(Engine.initialisation_board(),null);
+
 };
