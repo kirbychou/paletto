ff6e9e6cd8d69483679e35841d1f1634100273fb
Lu Scie <lucy.carpe@gmail.com>
2014-10-29 22:57:57 +0100

    First Story

--- src/Engine.js
+++ src/Engine.js
@@ -6,72 +6,74 @@ Engine = function () {
 // private attributes and methods
     var board = new Array(6);
     var line, column;
-    for (line = 1; line < 6; line = line + 1) {
+    for (line = 0; line < 7; line = line + 1) {
         board[line] = new Array(6);
     }
 
     this.check_length_board = function () {
-        return board.length* board.length;
+        return ((board.length - 1 )* (board.length - 1));
     };
 
     this.initialisation_board = function () {
-        board[0][1] = "Black";
-        board[0][2] = "Green";
-        board[0][3] = "White";
-        board[0][4] = "Blue";
-        board[0][5] = "Red";
-        board[0][6] = "White";
+        board[0][0] ="Black";
+        board[0][1] ="Green";
+        board[0][2] ="White";
+        board[0][3] ="Blue";
+        board[0][4] ="Red";
+        board[0][5] ="White";
 
-        board[1][1] = "Yellow";
-        board[1][2] = "White";
-        board[1][3] = "green";
-        board[1][4] = "Red";
-        board[1][5] = "Yellow";
-        board[1][6] = "Blue";
+        board[1][0] ="Yellow";
+        board[1][1] ="White";
+        board[1][2] ="Green";
+        board[1][3] ="Red";
+        board[1][4] ="Yellow";
+        board[1][5] ="Blue";
 
-        board[2][1] = "Blue";
-        board[2][2] = "Yellow";
-        board[2][3] = "Blue";
-        board[2][4] = "white";
-        board[2][5] = "Black";
-        board[2][6] = "Red";
+        board[2][0] ="Blue";
+        board[2][1] ="Yellow";
+        board[2][2] = "Blue";
+        board[2][3] = "White";
+        board[2][4] = "Black";
+        board[2][5] = "Red";
 
-        board[3][1] = "Red";
-        board[3][2] = "Black";
-        board[3][3] = "Red";
-        board[3][4] = "Green";
-        board[3][5] = "Blue";
-        board[3][6] = "White";
+        board[3][0] = "Red";
+        board[3][1] = "Black";
+        board[3][2] = "Red";
+        board[3][3] = "Green";
+        board[3][4] = "Blue";
+        board[3][5] = "White";
 
-        board[4][1] = "white";
-        board[4][2] = "green";
-        board[4][3] = "Yellow";
-        board[4][4] = "Black";
-        board[4][5] = "Yellow";
-        board[4][6] = "Green";
+        board[4][0] = "White";
+        board[4][1] = "Green";
+        board[4][2] = "Yellow";
+        board[4][3] = "Black";
+        board[4][4] = "Yellow";
+        board[4][5] = "Green";
+            
+        board[5][0] = "Yellow";
+        board[5][1] = "Blue";
+        board[5][2] = "Black";
+        board[5][3] = "Red";
+        board[5][4] = "Green";
+        board[5][5] = "Black";
 
-        board[5][1] = "Yellow";
-        board[5][2] = "Blue";
-        board[5][3] = "Black";
-        board[5][4] = "Red";
-        board[5][5] = "Green";
-        board[5][6] = "Black";
 
 
         var  juxtaposition =0;
-        var column = 1;
+
         var line = 1;
         do {
-
-            if (board[line][column] == board[line + 1][column]
-                || board[line][column] == board[line - 1][column]
-                || board[line][column] == board[line][column+1]
-                || board[line][column] == board[line][column-1]) {
-                juxtaposition = 1;
+            for(column =1; column< board.length-2; column++) {
+                if ((board[line][column] == board[line + 1][column])
+                    || (board[line][column] == board[line - 1][column])
+                    || (board[line][column] == board[line][column + 1])
+                    || (board[line][column] == board[line][column - 1])) {
+                    juxtaposition = 1;
+                }
             }
             line = line +1;
-            column = column +1;
-        } while (juxtaposition == 0 || column < 4 && line <4);
+
+        } while (line <5);
         return juxtaposition;
     };
 // public methods
